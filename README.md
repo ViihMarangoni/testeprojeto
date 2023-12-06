12f150f767b8c69b6fcc79682364128aef8c5bc9
ViihMarangoni <142364559+ViihMarangoni@users.noreply.github.com>
2023-12-05 23:54:49 -0300

    atualizações de casa

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# testeprojeto
- testando
+### Pimentas 🌶️ | Salames 🥓 | Queijos 🧀 | Carnes 🍖
+#
+### Sobre 🔍
+Somos um empório com muitas variedades, trazendo cada dia mais novidades e qualidades em nossos produtos.
+#
+### Contato 📲
+Fone: (19) 9.7417-4448 
+#
+### Introdução 
+- Vamos começar criando a primeira página do HTML, onde vai ter [queijos], [salames], [pimentas] e [carnes].
+
+- Na nossa primeira página tambem tem que haver "CONTATOS", "SOBRE", "INÍCIO".
+
--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-function Queijos(){
-    document.getElementById("queijo").innerText = "queijo"
-    document.getElementById("imagem").src="queijo.jpg"
-    document.getElementById("frase").innerText = " 'Eu Sei de Cor' é uma das musicas mais tocadas do cd"
-    document.body.style="background: #00BFFF"
+function abrirsalame() {
+    // Abrir a página queijo.html em uma nova janela ou guia
+    window.open('salame.html', '_blank');
+    // Fechar a janela ou guia atual (pode não funcionar em alguns navegadores devido a restrições de segurança)
+    window.close();
+}
+function abrirqueijo() {
+    window.open('queijo.html', '_blank');
+    window.close();
+}
+function abrirpimenta() {
+    window.open('pimenta.html', '_blank');
 }
