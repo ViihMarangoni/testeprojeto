6b60992b00a06c660368872006ed05c782cd086b
ViihMarangoni <vmarangoni113@icloud.com>
2023-12-05 22:05:18 -0300

    mais testes

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-function Realidade(){
-    document.getElementById("mensagem").innerText = "Lançamento: 6 de março de 2017"
+function Queijos(){
+    document.getElementById("queijo").innerText = "queijo"
+    document.getElementById("imagem").src="queijo.jpg"
     document.getElementById("frase").innerText = " 'Eu Sei de Cor' é uma das musicas mais tocadas do cd"
-    document.getElementById("imagem").src="rea.jpg"
     document.body.style="background: #00BFFF"
-}
-function TodososCantos(){
-    document.getElementById("mensagem").innerText = "Lançamento: 22 de fevereiro de 2019"
-    document.getElementById("frase").innerText = " 'Bem Pior Que Eu'foi uma das musicas mais tocadas do cd"
-    document.getElementById("imagem").src="todos.jpg"
-    document.body.style="background: #FFD700"
 }
